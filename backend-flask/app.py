8a4f4ed2b197a32ddce8a939066dedd558874135
Flor Perzefi <perzefi@gmail.com>
2024-01-18 15:10:21 +0000

    Implimented CloudWatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,12 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+#Added CloudWatch
+import watchtower
+import logging
+from time import strftime
+
+
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -14,21 +20,24 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+
+
 # HoneyComb OpenTelemetry ..
 
-from opentelemetry import trace
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+#from opentelemetry import trace
+#from opentelemetry.instrumentation.flask import FlaskInstrumentor
+#from opentelemetry.instrumentation.requests import RequestsInstrumentor
+#from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+#from opentelemetry.sdk.trace import TracerProvider
+#from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 #Addign Xray Tracing
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+#from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -37,6 +46,14 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+# Configuring Logger to Use CloudWatch
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG) 
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler(log_group="cruddur")
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
 
 
 app = Flask(__name__)
@@ -61,6 +78,13 @@ def data_message_groups():
     return model['errors'], 422
   else:
     return model['data'], 200
+
+#@app.after_request
+#def after_request(response):
+#   timestamp = strftime('[%Y-%b-%d %H:%M]')
+#   LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#   return response
+
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
